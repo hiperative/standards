a58731fbfb38d8c035c7feef4a9dc7a1630fc80f
Rafaell Lycan <sonny.webdsg@gmail.com>
2022-10-15 15:25:20 +0200

    Add missing ESLint rules and configure .eslintrc and .eslintignore on the repo

--- packages/eslint-config/typescript.js
+++ packages/eslint-config/typescript.js
@@ -1,55 +1,88 @@
 module.exports = {
   root: true,
-  extends: [
-    require.resolve('./base'),
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'prettier',
-    'prettier/@typescript-eslint',
-  ],
+  extends: [require.resolve('./base'), 'prettier'],
 
-  parser: '@typescript-eslint/parser',
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
 
-  plugins: ['@typescript-eslint'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
 
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-    // typescript-eslint specific options
-    warnOnUnsupportedTypeScriptVersion: true,
-  },
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
 
-  rules: {
-    // TypeScript's `noFallthroughCasesInSwitch` option is more robust (#6906)
-    'default-case': 'off',
-
-    // Uses @typescript-eslint/dot-notation instead
-    'dot-notation': 'off',
-
-    // TypeScript compilation already ensures that named imports exist in the referenced module
-    'import/named': 'off',
-
-    // @TYPESCRIPT PLUGIN
-    '@typescript-eslint/ban-ts-comment': 'off',
-
-    '@typescript-eslint/consistent-type-assertions': 'error',
-
-    '@typescript-eslint/dot-notation': 'error',
-
-    '@typescript-eslint/member-ordering': 'warn',
-
-    '@typescript-eslint/no-shadow': [
-      'warn',
-      {
-        hoist: 'all',
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: 'tsconfig.json',
+        // typescript-eslint specific options
+        warnOnUnsupportedTypeScriptVersion: true,
       },
-    ],
 
-    '@typescript-eslint/no-unused-expressions': 'warn',
+      rules: {
+        // TypeScript's `noFallthroughCasesInSwitch` option is more robust (#6906)
+        'default-case': 'off',
 
-    '@typescript-eslint/prefer-for-of': 'warn',
-  },
+        // TypeScript compilation already ensures that named imports exist in the referenced module
+        'import/named': 'off',
+
+        // @TYPESCRIPT PLUGIN
+        '@typescript-eslint/ban-ts-comment': 'off',
+
+        '@typescript-eslint/consistent-type-assertions': 'warn',
+
+        'dot-notation': 'off',
+        '@typescript-eslint/dot-notation': 'error',
+
+        '@typescript-eslint/member-ordering': 'warn',
+
+        'no-array-constructor': 'off',
+        '@typescript-eslint/no-array-constructor': 'warn',
+
+        'no-redeclare': 'off',
+        '@typescript-eslint/no-redeclare': 'error',
+
+        '@typescript-eslint/no-shadow': [
+          'error',
+          {
+            hoist: 'all',
+          },
+        ],
+
+        'no-unused-expressions': 'off',
+        '@typescript-eslint/no-unused-expressions': [
+          'error',
+          {
+            allowShortCircuit: true,
+            allowTernary: true,
+            allowTaggedTemplates: true,
+          },
+        ],
+
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': [
+          'warn',
+          {
+            functions: false,
+            classes: false,
+            variables: false,
+            typedefs: false,
+          },
+        ],
+
+        'no-useless-constructor': 'off',
+        '@typescript-eslint/no-useless-constructor': 'error',
+
+        '@typescript-eslint/prefer-for-of': 'warn',
+
+        '@typescript-eslint/type-annotation-spacing': 'warn',
+      },
+    },
+  ],
 }
